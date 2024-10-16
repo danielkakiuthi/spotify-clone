94de51151c82089e02e3e32c02d5db8eddf89e69
Daniel Kakiuthi <daniel.kakiuthi@gmail.com>
2024-10-15 18:14:19 -0600

    feat: added player functionality

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -2,12 +2,15 @@
 
 import { Song } from "@/types";
 import SongItem from "@/components/SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface PageContentProps {
   songs: Song[];
 }
 
 const PageContent: React.FC<PageContentProps> = ({ songs }) => {
+  const onPlay = useOnPlay(songs);
+
   if (songs.length === 0) {
     return <div className="mt-4 text-neutral-400">No Songs Available.</div>;
   }
@@ -26,7 +29,11 @@ const PageContent: React.FC<PageContentProps> = ({ songs }) => {
       "
     >
       {songs.map((item) => (
-        <SongItem key={item.id} onClick={() => {}} data={item} />
+        <SongItem
+          key={item.id}
+          onClick={(id: string) => onPlay(id)}
+          data={item}
+        />
       ))}
     </div>
   );
